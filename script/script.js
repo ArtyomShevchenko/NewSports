6559eb572f1a2b6505aa05d099a1280a838ecc80
ArtyomShevchenko <artyomshevchenkowarmgray@gmail.com>
2022-06-28 13:18:36 +0300

    fix many bags

--- script/script.js
+++ script/script.js
@@ -1,10 +1,24 @@
+class Form {
+    constructor(name, surname, email, message) {
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.message = message;
+    }
+}
+const formData = new Form();
+
+// variables
 const burger = document.querySelector(".burger");
 const menu = document.querySelector(".menu").children[0];
 const nav = document.querySelector(".navigation-container");
+const submitButton = document.querySelector(".submit-button")
+const form = document.querySelector("form")
+
 
 document.body.addEventListener("click", (event) => {
 
-    // click on burger and toggle class
+    // burger
     if (event.target === burger) {
         burger.classList.toggle(`_active`);
         menu.classList.toggle(`_active`);
@@ -16,12 +30,27 @@ document.body.addEventListener("click", (event) => {
         nav.classList.remove("navigation-container_active");
         document.body.classList.remove("open-menu-burger");
     };
-    // 
+
+
+    // form
+    if (event.target === submitButton) {
+        event.preventDefault();
+
+        if (form[4].checked === true) {
+            formData.name = form[0].value;
+            formData.surname = form[1].value;
+            formData.email = form[2].value;
+            formData.message = form[3].textContent;
+
+            console.log(JSON.stringify(formData))
+        }
+    }
 });
 
-document.querySelectorAll("button").forEach( button => {
+document.querySelectorAll(".post-button").forEach(button => {
     button.addEventListener("click", () => {
+        console.log(button);
         button.parentElement.classList.toggle("_active-post");
         nav.classList.toggle("_shrink");
     })
-})
+});
